f287bf947afd39921c636c622ba5f83a11f977d1
Sanjida Islam <sanjimo123@gmail.com>
2021-09-05 21:59:08 +0600

    validation done

--- weather.js
+++ weather.js
@@ -7,13 +7,20 @@ const setInnerText = (id, value) => {
 //searchTemperature function
 const searchTemperature = () => {
     const cityName = document.getElementById('city-name').value;
-    //weather API and API_KEY declaration
-    const api_key = "3e591ff5312669b3a539c8595bddf291";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}`;
-    //fetch
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayTemperature(data))
+    //input validation
+    if (cityName) {
+        //weather API and API_KEY declaration
+        const api_key = "3e591ff5312669b3a539c8595bddf291";
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api_key}&units=metric`;
+        //fetch
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displayTemperature(data))
+    }
+    else {
+        setInnerText('validation-check', 'Please enter a city name!');
+    }
+
 }
 
 
@@ -22,6 +29,8 @@ const displayTemperature = temperature => {
     console.log(temperature);
     //set city name
     setInnerText('city', temperature.name);
+    //set temperature
+    setInnerText('temperature', temperature.main.temp);
     //set condition
     setInnerText('condition', temperature.weather[0].main);
     //set weather-icon
